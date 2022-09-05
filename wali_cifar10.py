afc507d8d5e7b0eb832cc110f47d95f6c3071857
Charlie Cheng-Jie Ji <charliechengjieji@berkeley.edu>
2022-09-05 11:55:06 -0700

    add simclr pipeline WIP

--- wali_cifar10.py
+++ wali_cifar10.py
@@ -39,22 +39,23 @@ BETA2 = 0.9
 # dataset path
 CIFAR_PATH = r'~\torch\data\CIFAR10'
 
-# FIXME - New training pipeline uses SimClr encoder.
-
 def create_generator():
   return Generator(IMAGE_SIZE, H_DIM, 8)
 
 def create_critic():
   x_mapping = Discriminator(IMAGE_SIZE)
-
+  # FIXME -  Question: what is DIM?
+  # kernal size is 1, stride is 1, padding is 0. 
   z_mapping = nn.Sequential(
     Conv2d(NLAT, 512, 1, 1, 0), LeakyReLU(LEAK),
     Conv2d(512, 512, 1, 1, 0), LeakyReLU(LEAK))
   
+  # DIM_D = 2048, dimension of the output of the discriminator
   joint_mapping = nn.Sequential(
     Conv2d(DIM_D + 512, 1024, 1, 1, 0), LeakyReLU(LEAK),
     Conv2d(1024, 1024, 1, 1, 0), LeakyReLU(LEAK),
     Conv2d(1024, 1, 1, 1, 0))
+  # last output channel is 1, output 0, 1.
 
   return JointCritic(x_mapping, z_mapping, joint_mapping)
 
