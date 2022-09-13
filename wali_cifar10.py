7a238e020cd6fb2e60d8c9eaad0eb59a42621c5a
Charlie Cheng-Jie Ji <charliechengjieji@berkeley.edu>
2022-09-13 12:48:53 -0700

    model save

--- wali_cifar10.py
+++ wali_cifar10.py
@@ -75,7 +75,7 @@ def create_WALI():
 
 # Training pipeline function
 def main():
-  logging.basicConfig(filename='run3.log', level=logging.DEBUG)
+  logging.basicConfig(filename='run4.log', level=logging.DEBUG)
   logging.info('Start training')
   writer = SummaryWriter("runs/cifar10")
 
