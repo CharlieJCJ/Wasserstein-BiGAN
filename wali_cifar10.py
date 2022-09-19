a4917e0beb927eedf0e58c313c236b0ad0e29d5f
Charlie Cheng-Jie Ji <charliechengjieji@berkeley.edu>
2022-09-19 02:45:34 -0700

    updated old

--- wali_cifar10.py
+++ wali_cifar10.py
@@ -12,40 +12,14 @@ from torch.cuda.amp import GradScaler, autocast
 from stylegan2 import Generator, Discriminator
 from torch.utils.tensorboard import SummaryWriter
 import logging
-from constants import *
-from models import create_WALI
 import click
+from constants import *
 
 WRITER_ITER = 10
 cudnn.benchmark = True
 torch.manual_seed(1)
 torch.cuda.manual_seed_all(1)
 
-
-# training hyperparameters
-N_VIEW = 2
-BATCH_SIZE = 16 # Original = 256, we start with something smaller
-ITER = 200000 # Number of epochs to train for
-IMAGE_SIZE = 32
-NUM_CHANNELS = 3
-H_DIM = 512
-Z_DIM = 128
-NLAT = 512
-DIM_D = 8192 # Need to check the size in stylegan2.py using test()
-
-LEAK = 0.2
-
-# FIXME
-DIM = 128
-C_ITERS = 5       # critic iterations
-EG_ITERS = 1      # encoder / generator iterations
-LAMBDA = 10       # strength of gradient penalty
-LEARNING_RATE = 1e-4
-BETA1 = 0.5
-BETA2 = 0.9
-
-# dataset path
-CIFAR_PATH = r'~\torch\data\CIFAR10'
 
 def create_generator():
   return Generator(IMAGE_SIZE, H_DIM, 8)
