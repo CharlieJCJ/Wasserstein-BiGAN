ab65acd67f7bd6ca1e7392c39f2279331ae03893
Charlie Cheng-Jie Ji <charliechengjieji@berkeley.edu>
2022-09-19 01:55:49 -0700

    update constants

--- constants.py
+++ constants.py
@@ -1,18 +1,17 @@
 
 # training hyperparameters
 N_VIEW = 2
-BATCH_SIZE = 2 # Original = 256, we start with something smaller
+BATCH_SIZE = 16 # Original = 256, we start with something smaller
 ITER = 200000 # Number of epochs to train for
 IMAGE_SIZE = 32
 NUM_CHANNELS = 3
 H_DIM = 512
 Z_DIM = 128
 NLAT = 512
-DIM_D = 8192 # Need to check the size in stylegan2.py using test()
+DIM_D = 8192 # Need to check the size in stylegan2.py using test(); checked
 
 LEAK = 0.2
 
-# FIXME
 DIM = 128
 C_ITERS = 5       # critic iterations
 EG_ITERS = 1      # encoder / generator iterations
@@ -25,3 +24,6 @@ BETA2 = 0.9
 CIFAR_PATH = r'~\torch\data\CIFAR10'
 
 WRITER_ITER = 10
+
+# Baseline setting
+baseline = True
