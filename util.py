2b713b54be4db0424da2ae733bea8e41885928e2
Charlie Cheng-Jie Ji <charliechengjieji@berkeley.edu>
2022-09-19 02:18:14 -0700

    a

--- util.py
+++ util.py
@@ -219,7 +219,7 @@ class WALI(nn.Module):
                                                                 # We don't need z_hat in this case.
     # print(h_hat.shape, z_hat.shape, x_tilde.shape)
     if baseline: 
-      data_preds, sample_preds = self.criticize(original_imgs, h_hat, x_tilde, h) 
+      data_preds, sample_preds = self.criticize(original_imgs.double(), h_hat.double(), x_tilde.double(), h.double()) 
       EG_loss = torch.mean(data_preds - sample_preds)
       C_loss = -EG_loss + lamb * self.calculate_grad_penalty(original_imgs.data, h_hat.data, x_tilde.data, h.data)
       return C_loss , EG_loss
