918611a0a521f514f95823d1215e7b368a9e8a28
CharlieJCJ <charliechengjieji@berkeley.edu>
2022-09-26 16:07:02 -0700

    A

--- constants.py
+++ constants.py
@@ -1,37 +1,37 @@
 #-> Dataset specify
-# dataset = "mnist"
+dataset = "cifar10"
 
 #-> Baseline setting
-# baseline = True # -- click
+baseline = True # -- click
 
 #-> Multiple GPU setting
-# CUDA_VISIBLE_DEVICES = '0, 1, 2, 3'
-# GPUS = 4              # -- click
-# N_VIEW = 2            # -- click default 2
+CUDA_VISIBLE_DEVICES = '0, 1, 2, 3'
+GPUS = 4              # -- click
+N_VIEW = 2            # -- click default 2
 
 #-> Hyperparameters (included in click)
-# BATCH_SIZE = 16 * 4   # Original = 256, we start with something smaller
-# ITER = 200000         # Number of epochs to train for
-# H_DIM = 512
-# Z_DIM = 128
-# NLAT = 512 # same as H_DIM
-# LEAK = 0.2
-# C_ITERS = 1           # critic iterations
-# EG_ITERS = 1          # encoder / generator iterations
-# LAMBDA = 10           # strength of gradient penalty
-# LEARNING_RATE = 1e-5  # Original in paper = 1e-4
-# BETA1 = 0.5
-# BETA2 = 0.9
-# DIM = 128             # Network dimension
+BATCH_SIZE = 16 * 4   # Original = 256, we start with something smaller
+ITER = 200000         # Number of epochs to train for
+H_DIM = 512
+Z_DIM = 128
+NLAT = 512 # same as H_DIM
+LEAK = 0.2
+C_ITERS = 1           # critic iterations
+EG_ITERS = 1          # encoder / generator iterations
+LAMBDA = 10           # strength of gradient penalty
+LEARNING_RATE = 1e-5  # Original in paper = 1e-4
+BETA1 = 0.5
+BETA2 = 0.9
+DIM = 128             # Network dimension
 
 #-> Writer settings
-# WRITER_ITER = 10      # log every 10 iterations
+WRITER_ITER = 10      # log every 10 iterations
 
 #-> Visualization settings
-# VISUAL_NUM = 8        # number of images to visualize
+VISUAL_NUM = 8        # number of images to visualize
 
-# if dataset == "cifar10":
-#     # training hyperparameters
-#     IMAGE_SIZE = 32
-#     NUM_CHANNELS = 3
-#     DIM_D = 8192        # Need to check the size in stylegan2.py using test(); checked
+if dataset == "cifar10":
+    # training hyperparameters
+    IMAGE_SIZE = 32
+    NUM_CHANNELS = 3
+    DIM_D = 8192        # Need to check the size in stylegan2.py using test(); checked
