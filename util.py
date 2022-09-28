15e22edd0938a98cc2ea10cb5ffe0b9ec3252732
CharlieJCJ <charliechengjieji@berkeley.edu>
2022-09-27 17:23:10 -0700

    dimension issue fixed

--- util.py
+++ util.py
@@ -381,9 +381,13 @@ class ContrastiveLearningViewGenerator(object):
         self.n_views = n_views
 
     def __call__(self, x):
+        # transform = transforms.Compose([
+        #     transforms.ToTensor(),
+        #     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
         transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+          transforms.ToTensor(),
+          transforms.Normalize((0.5, 0.5))])
         # print("My length is", len([self.base_transform(x) for i in range(self.n_views)] + [transform(x)]))
         # return [self.base_transform(x) for i in range(self.n_views)]
         return self.base_transform(x), self.base_transform(x), transform(x) # dataloader handles the rest
