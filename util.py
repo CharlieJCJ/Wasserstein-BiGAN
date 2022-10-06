7c775f1c62b2d61cc19a293143ea09e9d64e30d7
CharlieJCJ <charliechengjieji@berkeley.edu>
2022-10-06 01:38:42 -0700

    tensor now

--- util.py
+++ util.py
@@ -355,7 +355,7 @@ class ContrastiveLearningDataset:
                           'LSUN': lambda: datasets.LSUN(self.root_folder, classes=['bedroom_train'], 
                                                                   transform=ContrastiveLearningViewGenerator(
                                                                   self.get_simclr_pipeline_transform(256),
-                                                                  n_views))
+                                                                  n_views, size=256))
                                                             }
 # transform=ContrastiveLearningViewGenerator(self.get_simclr_pipeline_transform(96), n_views),
         try:
@@ -378,9 +378,10 @@ class InvalidDatasetSelection(BaseSimCLRException):
 class ContrastiveLearningViewGenerator(object):
     """Take two random crops of one image as the query and key."""
 
-    def __init__(self, base_transform, n_views=2):
+    def __init__(self, base_transform, n_views=2, size=32):
         self.base_transform = base_transform
         self.n_views = n_views
+        self.size = size
 
     def __call__(self, x):
         # transform = transforms.Compose([
@@ -389,7 +390,7 @@ class ContrastiveLearningViewGenerator(object):
         x = x.convert('RGB')
         
         transform = transforms.Compose([
-          transforms.Resize(size=32),
+          transforms.Resize(size=self.size),
           transforms.ToTensor(),
           transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))])
         # print("My length is", len([self.base_transform(x) for i in range(self.n_views)] + [transform(x)]))
