bb478361ee7c1fdefc1deeb9b8d056a312540935
CharlieJCJ <charliechengjieji@berkeley.edu>
2022-10-06 01:41:59 -0700

    tensor now

--- util.py
+++ util.py
@@ -354,8 +354,8 @@ class ContrastiveLearningDataset:
                                                               download=True),
                           'LSUN': lambda: datasets.LSUN(self.root_folder, classes=['bedroom_train'], 
                                                                   transform=ContrastiveLearningViewGenerator(
-                                                                  self.get_simclr_pipeline_transform(256),
-                                                                  n_views, size=256))
+                                                                  self.get_simclr_pipeline_transform(128),
+                                                                  n_views, size=128))
                                                             }
 # transform=ContrastiveLearningViewGenerator(self.get_simclr_pipeline_transform(96), n_views),
         try:
